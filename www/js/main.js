5f79daa5c468968d6e2d6476d41a5f6585a5d68c
cdynak <cezary.dynak@gmail.com>
2015-11-29 17:13:48 +0100

    initial commit

---
+++ www/js/main.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * Bootstrap one-page template with Parallax effect | Script Tutorials
+ * http://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+$(document).ready(function (){
+
+  // create a LatLng object containing the coordinate for the center of the map
+  var latlng = new google.maps.LatLng(50.6025134,23.1217477);
+
+  // prepare the map properties
+  var options = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: true,
+    mapTypeControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true
+  };
+
+  // initialize the map object
+  var map = new google.maps.Map(document.getElementById('google_map'), options);
+
+  // add Marker
+  var marker1 = new google.maps.Marker({
+    position: latlng, map: map
+  });
+
+  // add listener for a click on the pin
+  google.maps.event.addListener(marker1, 'click', function() {
+    infowindow.open(map, marker1);
+  });
+
+  // add information window
+  var infowindow = new google.maps.InfoWindow({
+    content:  '<div class="info"><strong>Żródło</strong><br><br>To tutaj żył nasz najstarszy znany przodek, Bazyli Deynak<br>Bliżów, Polska</div>'
+  });  
+});
